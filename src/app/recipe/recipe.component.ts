54e263d7e9fcf1e9fcee25d6910b52a6229e97cb
Luis Felipe Enriquez <lenriquez@alertlogic.com>
2016-11-30 08:39:10 -0500

    Adding header component and recipe component

---
+++ src/app/recipe/recipe.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rb-recipe',
+  templateUrl: './recipe.component.html'
+})
+export class RecipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
