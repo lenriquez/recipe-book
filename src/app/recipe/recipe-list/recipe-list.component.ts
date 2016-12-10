94aae3bf80b91c3bb135af7d766b31e0e106bf04
Luis Felipe Enriquez <lenriquez@alertlogic.com>
2016-12-10 11:43:07 -0500

    Adding my own directives and Ingredients

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -7,9 +7,12 @@ import { RecipeItemComponent } from './recipe-item.component';
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'Very tasty', 'http://images.derberater.de/files/imagecache/456xXXX_berater/berater/slides/WienerSchnitzel.jpg', []),
+    new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipe = new Recipe('Dummy', 'Dummy','http://vignette3.wikia.nocookie.net/scribblenauts/images/7/75/Crash_Test_Dummy_Female.png/revision/latest?cb=20130309213419');
+  // recipe = new Recipe('Dummy', 'Dummy','http://vignette3.wikia.nocookie.net/scribblenauts/images/7/75/Crash_Test_Dummy_Female.png/revision/latest?cb=20130309213419');
 
   constructor() { }
 
